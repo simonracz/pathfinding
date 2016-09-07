3360b78df84c8b20061f4badf9fe7066cbd04177
Simon Racz <simonracz@gmail.com>
2016-09-07 16:26:17 +0200

    Refactored cmd line options parser.

--- generate.cpp
+++ generate.cpp
@@ -2,56 +2,52 @@
 #include <algorithm>
 #include "cxxopts.hpp"
 
-class options {
+class cmd_options {
 private:
-	int n = 0;
+	int n = 5;
 	bool hexagonal = false;
 	bool square = false;
 	bool help = false;
-	std::string help_msg = "";
-public:
-	options() {}
-	bool parse_options(int argc, char* argv[]) {
+	cxxopts::Options options;
+	bool parse(int argc, char* argv[]) {
 		try {
-			// Setting up cmd line options
-			cxxopts::Options options(argv[0], " - Generator of simple maps");
-			options.add_options()
-		  	  ("n,size", "size of the map", cxxopts::value<int>()
-		  		->default_value("5"))
-		  	  ("x,hexagonal", "generate hexagonal maps", cxxopts::value<bool>())
-		  	  ("s,square", "generate square maps", cxxopts::value<bool>())
-		  	  ("h,help", "Prints help", cxxopts::value<bool>())
-			;
-
-			help_msg = options.help({""});
-
-			// Parsing options
-			options.parse(argc, argv);			
-
-			help = options["help"].as<bool>();
-			hexagonal = options["hexagonal"].as<bool>();
-			square = options["square"].as<bool>();
-			n = options["size"].as<int>();
-
-			// Ensuring consistency
-			n = std::max(1, n);
-			if (square && hexagonal) {
-				square = false;
-			}
-			if (!square && !hexagonal) {
-				hexagonal = true;
-			}
+			options.parse(argc, argv);
 			return true;
-		} catch (const cxxopts::OptionException& e) {
-			std::cout << "error parsing options: " << e.what() << std::endl;
+		} catch(const cxxopts::OptionException& e) {
+			std::cerr << "error parsing options: " << e.what() << std::endl;
 			return false;
 		}
+	}
+	void ensure_consistency() {
+		n = std::max(1, n);
+		if (square && hexagonal) {
+			square = false;
+		}
+		if (!square && !hexagonal) {
+			hexagonal = true;
+		}
+	}
+public:
+	cmd_options() : options("generate", " - Generator of simple maps") {
+		options.add_options()
+	  	  ("n,size", "size of the map", cxxopts::value<int>(n))
+	  	  ("x,hexagonal", "generate hexagonal maps", cxxopts::value<bool>(hexagonal))
+	  	  ("s,square", "generate square maps", cxxopts::value<bool>(square))
+	  	  ("h,help", "Prints help", cxxopts::value<bool>(help))
+		;
+	}
+	bool parse_cmd_line(int argc, char* argv[]) {
+		if (!parse(argc, argv)) {
+			return false;
+		}
+		ensure_consistency();
+		return true;
 	}
 	int size() {return n;}
 	bool isHexagonal() {return hexagonal;}
 	bool isSquare() {return square;}
 	bool isHelp() {return help;}
-	const std::string& help_message() {return help_msg;}
+	std::string help_message() {return options.help({""});}
 	void print() {
 		std::cout << "options = {" 
 				  << "\n  size: " << n
@@ -63,8 +59,8 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-	options opts;
-	bool ret = opts.parse_options(argc, argv);
+	cmd_options opts;
+	bool ret = opts.parse_cmd_line(argc, argv);
 	if (!ret) {
 		std::cout << opts.help_message() << std::endl;
 		return 1;
